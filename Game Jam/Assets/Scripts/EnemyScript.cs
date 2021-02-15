91fbd40cba4602289bdc1675de735c3b7e34712e
BuildTools <unconfigured@null.spigotmc.org>
2021-02-14 16:32:49 -0800

    Changed project version

---
+++ Game Jam/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public Transform playerTransform;
+    public int maxDistanceToAgress;
+    public int speed;
+    public Rigidbody2D rb;
+    public int damage;
+
+    private void FixedUpdate() {
+        float distance = Mathf.Sqrt(Mathf.Pow(transform.position.x - playerTransform.position.x, 2) + Mathf.Pow(transform.position.y - playerTransform.position.y, 2) + Mathf.Pow(transform.position.z - playerTransform.position.z, 2));
+        if (distance <= maxDistanceToAgress) {
+            if (transform.position.x - playerTransform.position.x < 0) {
+                rb.velocity = new Vector2(rb.velocity.x + speed, 0);
+                if (rb.velocity.x > 1) {
+                    rb.velocity = new Vector2(rb.velocity.x - 1, 0);
+                }
+            } else {
+                rb.velocity = new Vector2(rb.velocity.x + -speed, 0);
+                if (rb.velocity.x < -1) {
+                    rb.velocity = new Vector2(rb.velocity.x + 1, 0);
+                }
+            }
+        } else {
+            rb.velocity = new Vector2(0, 0);
+        }
+    }   
+}

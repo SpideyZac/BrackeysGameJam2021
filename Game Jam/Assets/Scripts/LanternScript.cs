8140aac8dfde9a98da736b2e7d27da68fe3e29a9
BuildTools <unconfigured@null.spigotmc.org>
2021-02-14 19:11:32 -0800

    Changed project version

---
+++ Game Jam/Assets/Scripts/LanternScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanternScript : MonoBehaviour
+{
+    public SpriteRenderer erenderer;
+    private Transform playerTransform;
+    public int maxDistance;
+    public string enemytags;
+    private bool beenToggled = false;
+    private PlayerMovement player;
+    public GameObject firefly;
+    private GameObject newInstance;
+    private Light flyLight;
+    private GameObject withTag;
+
+    private void Awake() {
+        player = GameObject.Find("player").GetComponent<PlayerMovement>();
+        playerTransform = GameObject.Find("player").GetComponent<Transform>();
+        erenderer.color = new Color(1f, 1f, 1f, 0f);
+    }
+
+    private void Update() {
+        withTag = GameObject.FindWithTag(enemytags);
+        if (Mathf.Sqrt(Mathf.Pow(transform.position.x - playerTransform.position.x, 2) + Mathf.Pow(transform.position.y - playerTransform.position.y, 2) + Mathf.Pow(transform.position.z - playerTransform.position.z, 2)) <= maxDistance && !beenToggled && player.fireflys > 0 && withTag == null) {
+            erenderer.color = new Color(1f, 1f, 1f, 1f);
+            if (Input.GetKeyDown(KeyCode.E) && !beenToggled && player.fireflys > 0) {
+                player.fireflys -= 1;
+                beenToggled = true;
+                newInstance = Instantiate(firefly, transform.position, new Quaternion(0, 0, 0, 0), transform);
+                newInstance.GetComponent<Light>().range = 4;
+                flyLight = newInstance.GetComponent<Light>();
+            }
+        } else {
+            erenderer.color = new Color(1f, 1f, 1f, 0f);
+        }
+
+        if (beenToggled) {
+            if (Mathf.Sqrt(Mathf.Pow(transform.position.x - playerTransform.position.x, 2) + Mathf.Pow(transform.position.y - playerTransform.position.y, 2) + Mathf.Pow(transform.position.z - playerTransform.position.z, 2)) <= flyLight.range && player.playerLight < player.maxPlayerLight) {
+                player.playerLight += 0.02;
+            }
+        }
+    }
+}
---
+++ Game Jam/Assets/Scripts/UIrenderer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UIrenderer : MonoBehaviour
+{
+    private Text text;
+    private PlayerMovement playerscript;
+
+    private void Awake() {
+        text = GetComponent<Text>();
+        playerscript = GameObject.Find("player").GetComponent(typeof(PlayerMovement)) as PlayerMovement;
+    }
+
+    private void Update() {
+        text.text = "Souls: " + playerscript.souls + " Fire Flys: " + playerscript.fireflys + " Light: " + playerscript.playerLight;
+    }
+}
